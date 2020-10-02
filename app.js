9add7cd67119c1473eb5cd160c963145b88bc21c
pin4eva <pin4eva@hotmail.com>
2020-10-02 15:38:39 +0100

    First commit

--- app.js
+++ app.js
@@ -1,29 +1,37 @@
-// // Javascript Types
-// var colors = ["red", "blue", "yellow"];
-// let db = { name: "Peter", gender: "Male", age: null };
-// const firstName = "Peter";
-// const last_name = "Akaliro";
-// const name = `${firstName} ${last_name}`;
-// let num = 30;
-// num = "30";
-// let x = 4,
-//   y = 6,
-//   z = x * y;
+// const num = "30";
+// num = num - 10;
+// num = num + 5;
+// // num = num / 2;
+// num = num % 2;
 
-// console.log(`My name is ${name} and i am ${z} years old`);
+const firstName = "Peter";
+const lastName = "Akaliro";
+const name = `My last name is ${lastName} and my first name is ${firstName}`;
 
-// const st1 = "me";
-// const string = "my string";
+console.log(name);
+// var alpha = "A B C D";
+// var num = 30;
+// var score = 2.7;
+// var isActive = false;
+// var colors = ["blue", "yellow", "red", 10, 3.9];
+// var profile = {
+//     name: "Peter Akaliro",
+//     email: "pin4eva@gmail.com",
+//     phone: "07062275085",
+//     age: null
+// };
 
-// /**
-//  * String
-//  * Number
-//  * float
-//  * Boolean
-//  * Array
-//  * Object
-//  * null
-//  * undefined
-//  */
+/*
+       JAVASCRIPT TYPES
 
-// // var name = "Godswill";
+       String
+       Number
+       Float
+       Boolean
+       Array
+       Object
+       Null
+       Undefined
+
+
+        **/
