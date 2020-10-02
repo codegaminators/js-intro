68dde131bf0f0206f4d2efdcde9a98a27a65be77
pin4eva <pin4eva@hotmail.com>
2020-10-02 13:06:14 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+// // Javascript Types
+// var colors = ["red", "blue", "yellow"];
+// let db = { name: "Peter", gender: "Male", age: null };
+// const firstName = "Peter";
+// const last_name = "Akaliro";
+// const name = `${firstName} ${last_name}`;
+// let num = 30;
+// num = "30";
+// let x = 4,
+//   y = 6,
+//   z = x * y;
+
+// console.log(`My name is ${name} and i am ${z} years old`);
+
+// const st1 = "me";
+// const string = "my string";
+
+// /**
+//  * String
+//  * Number
+//  * float
+//  * Boolean
+//  * Array
+//  * Object
+//  * null
+//  * undefined
+//  */
+
+// // var name = "Godswill";
